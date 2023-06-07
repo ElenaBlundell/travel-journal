be693c1a7964531aef1d8d9e9a952cdc9c5809a2
Elena Blundell <57737705+ElenaBlundell@users.noreply.github.com>
2023-06-08 02:14:55 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Header(){
     return <header>
-            <img src="./images/globe-icon.png" />
+            <img src="images/globe-icon.png" />
             <h2>my travel journal.</h2>
           </header>
 }
